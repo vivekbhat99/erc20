560553f0aa4fd3177722d60e6507cec39ffd5ce8
vivek <vbhat99999@gmail.com>
2020-09-20 19:03:20 +0530

    transfer test

--- erc20Token/contracts/EveToc.sol
+++ erc20Token/contracts/EveToc.sol
@@ -9,13 +9,22 @@ contract EveToc {
     //balanceOf()- as defined in erc20 token standard-takes address and gives uint256 basically balance in the address
     mapping (address => uint256) public balanceOf;
 
+    event Transfer(address indexed _from,address indexed _to, uint256 _value );
+
     constructor (uint256 _initialTotalSupply) public {
         balanceOf[msg.sender] = _initialTotalSupply;
         totalSupply = _initialTotalSupply; 
     }
 
-
-     
+    // trasnsfer function to send 
+     function transfer(address _to, uint256 _value) public returns (bool success){
+        //require(balanceOf[msg.sender] != 0, 'No balance'); 
+        require(balanceOf[msg.sender] >= _value, 'transaction revert');
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+     }
 
 
 
--- erc20Token/test/EveToc.js
+++ erc20Token/test/EveToc.js
@@ -36,3 +36,30 @@ it('allocates the initial supply upon deployment', function() {
     });
   });
 });
+
+
+it('transfer success', function(){
+    return Eve.deployed().then(function(instance){
+        tokenInstance = instance;
+        // testing "require" first by sending larger than the senders balance
+        return tokenInstance.transfer.call(accounts[1], 9999999999999);
+    }).then(assert.fail).catch(function(error){
+         assert(error.message.indexOf('revert' >= 0, 'error message must contain revert'));
+         return tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] });
+    }).then(function(success){
+        assert.equal(success, true,'returns true');
+        return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] });
+    }).then(function(receipt){
+        assert.equal(receipt.logs[0].length,1, 'transfer one event');
+        assert.equal(receipt.logs[0].event, 'Transfer', 'the "Transfer" event');
+        assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account thr tokens are transferred from');
+        assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account thr token are transferred to');
+        assert.equal(receipt.logs[0].args._value,250000, 'logs the transfer amount');
+        return tokenInstance.balanceOf(accounts[1]);
+    }).then(function(balance){
+        assert.equal(balance.toNumber(), 250000,'adds amount to recevers account');
+        return tokenInstance.balanceOf(accounts[0]);
+    }).then(function(balance){
+        assert.equal(balance.toNumber(), (10000000 - 250000),'remaining balance of senders account');
+    });
+});
