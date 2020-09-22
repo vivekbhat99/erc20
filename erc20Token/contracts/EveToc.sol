59b86183470b511f3740dd9a504633b08c1e8e17
vivek <vbhat99999@gmail.com>
2020-09-22 21:13:37 +0530

    completed as per standards + testing

--- erc20Token/contracts/EveToc.sol
+++ erc20Token/contracts/EveToc.sol
@@ -5,27 +5,49 @@ contract EveToc {
     uint256 public totalSupply;
     string public name = "Eve Token";
     string public symbol = "EVE";
-    string public standard = "EVE Token v1"; 
+    string public standard = "EVE Token v1";
+
     //balanceOf()- as defined in erc20 token standard-takes address and gives uint256 basically balance in the address
-    mapping (address => uint256) public balanceOf;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
-    event Transfer(address indexed _from,address indexed _to, uint256 _value );
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address _spender, uint256 _value);
 
-    constructor (uint256 _initialTotalSupply) public {
+
+    constructor(uint256 _initialTotalSupply) public {
         balanceOf[msg.sender] = _initialTotalSupply;
-        totalSupply = _initialTotalSupply; 
+        totalSupply = _initialTotalSupply;
     }
 
     // trasnsfer function to send 
-     function transfer(address _to, uint256 _value) public returns (bool success){
+    function transfer(address _to, uint256 _value) public returns(bool success) {
         //require(balanceOf[msg.sender] != 0, 'No balance'); 
-        require(balanceOf[msg.sender] >= _value, 'transaction revert');
+        require(balanceOf[msg.sender] >= _value, 'insufficient balance = revert');
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
         emit Transfer(msg.sender, _to, _value);
         return true;
-     }
+    }
 
+    // trasnfer without sender initiating transfer i.e (delegated transfer) 
+    // transferFrom, approve, allowance, log approval
+
+    function approve(address _spender, uint256 _value) public returns(bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success) {
+        require(balanceOf[_from] >= _value, 'insufficient balance in spenders account = revert');
+        require(allowance[_from][msg.sender] >= _value, 'insufficient required allowance = revert');
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
 
 
 }
--- erc20Token/test/EveToc.js
+++ erc20Token/test/EveToc.js
@@ -35,10 +35,10 @@ it('allocates the initial supply upon deployment', function() {
       assert.equal(initialToc.toNumber(), 10000000, 'allocates the initial supply to account');
     });
   });
-});
 
 
-it('transfer success', function(){
+
+it('transfer', function(){
     return Eve.deployed().then(function(instance){
         tokenInstance = instance;
         // testing "require" first by sending larger than the senders balance
@@ -50,7 +50,7 @@ it('transfer success', function(){
         assert.equal(success, true,'returns true');
         return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] });
     }).then(function(receipt){
-        assert.equal(receipt.logs[0].length,1, 'transfer one event');
+        assert.equal(receipt.logs.length,1, 'trigger only one event');
         assert.equal(receipt.logs[0].event, 'Transfer', 'the "Transfer" event');
         assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account thr tokens are transferred from');
         assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account thr token are transferred to');
@@ -63,3 +63,91 @@ it('transfer success', function(){
         assert.equal(balance.toNumber(), (10000000 - 250000),'remaining balance of senders account');
     });
 });
+
+it ('approves tokens for delagated transfer', function(){
+    return Eve.deployed().then(function(instance){
+        tokenInstance = instance;
+        return tokenInstance.approve.call(accounts[1], 100);
+    }).then(function(success){
+        assert.equal(success, true, 'returns true');
+        return tokenInstance.approve(accounts[1], 100, { from: accounts[0]});
+    }).then(function(receipt){
+        assert.equal(receipt.logs.length,1, 'trigger only one event');
+        assert.equal(receipt.logs[0].event, 'Approval', 'the "Approval" event');
+        assert.equal(receipt.logs[0].args._owner, accounts[0], 'logs the account thr tokens are transferred from');
+        assert.equal(receipt.logs[0].args._spender, accounts[1], 'logs the account thr token are transferred to');
+        assert.equal(receipt.logs[0].args._value,100, 'logs the transfer amount');
+        return tokenInstance.allowance(accounts[0], accounts[1]);
+    }).then(function(allowance){
+        assert.equal(allowance.toNumber(), 100, 'storing allowance for delagated transfer');
+    });
+});
+
+it('handles delegated token transfers', function() {
+    return Eve.deployed().then(function(instance) {
+      tokenInstance = instance;
+      _fromAccount = accounts[2];
+      _toAccount = accounts[3];
+      _spendingAccount = accounts[4];
+      // Transfer some tokens to fromAccount
+      return tokenInstance.transfer(_fromAccount, 100, { from: accounts[0] });
+    }).then(function(receipt) {
+      // Approve spendingAccount to spend 10 tokens form fromAccount
+      return tokenInstance.approve(_spendingAccount, 50, { from: _fromAccount });
+    }).then(function(receipt) {
+      // Try transferring something larger than the sender's balance
+      return tokenInstance.transferFrom(_fromAccount, _toAccount, 9999, { from: _spendingAccount });
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, 'cannot transfer cuz insufficient balance');
+      // Try transferring something larger than the approved amount
+      return tokenInstance.transferFrom(_fromAccount, _toAccount, 70, { from: _spendingAccount });
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, 'cannot transfer cuz value more approved');
+      return tokenInstance.transferFrom.call(_fromAccount, _toAccount, 50, { from: _spendingAccount });
+    }).then(function(success) {
+      assert.equal(success, true);
+      return tokenInstance.transferFrom(_fromAccount, _toAccount, 50, { from: _spendingAccount });
+    }).then(function(receipt) {
+      assert.equal(receipt.logs.length, 1, 'triggers one event');
+      assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
+      assert.equal(receipt.logs[0].args._from, _fromAccount, 'logs the account the tokens are transferred from');
+      assert.equal(receipt.logs[0].args._to, _toAccount, 'logs the account the tokens are transferred to');
+      assert.equal(receipt.logs[0].args._value, 50, 'logs the transfer amount');
+      return tokenInstance.balanceOf(_fromAccount);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), 50, 'deducts the amount from the sending account');
+      return tokenInstance.balanceOf(_toAccount);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), 50, 'adds the amount from the receiving account');
+      return tokenInstance.allowance(_fromAccount, _spendingAccount);
+    }).then(function(allowance) {
+      assert.equal(allowance.toNumber(), 0, 'deducts the amount from the allowance');
+    });
+  });
+});
+
+
+// it('transferFrom - delagated transfer', function(){
+//     return Eve.deployed().then(function(instance){
+//         tokenInstance = instance;
+//         _fromAccount = accounts[2];
+//         _toAccount = accounts[3];
+//         _spendingAccount = accounts[4];
+//         //transfering some tokens to _fromAccount
+//         return tokenInstance.transferFrom(_fromAccount, 100, { from: accounts[0]});
+//     }).then(function(receipt){
+//         // approve 50 tokens from _spendingAccount
+//         return tokenInstance.approve(_spendingAccount, 20, {from: _fromAccount});
+//     }).then(function(receipt){
+//         //try transfering larger amount 
+//         return tokenInstance.transferFrom(_fromAccount, _toAccount, 99999, {from: _spendingAccount});
+//     }).then(assert.fail).catch(function(error){
+//         assert(error.message.indexOf('revert') >= 0, 'cannot transfer cuz  larger than balance');
+//         // testing transfer larger than approved amount (allowance)
+//         return tokenInstance.transferFrom(_fromAccount, toAccount, 30, {from: _spendingAccount});
+//     }).then(assert.fail).catch(function(error){
+//         assert(error.message.indexOf('revert') >= 0, 'cannot transfer cuz  larger approved amount (allownce)');
+// });
+
+// });
+
